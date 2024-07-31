b65caa130da18294528f9a53bde86b66b3ce9f4c
Alison Tshala <alison.tshala@capco.com>
2024-07-31 01:09:38 +0100

    log out the poison pill msg as string

--- src/main/java/com/tshalaa1/ErrorMessageRecoverer.java
+++ src/main/java/com/tshalaa1/ErrorMessageRecoverer.java
@@ -2,12 +2,14 @@ package com.tshalaa1;
 
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.listener.ConsumerRecordRecoverer;
+import org.springframework.kafka.support.serializer.DeserializationException;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ErrorMessageRecoverer implements ConsumerRecordRecoverer {
     @Override
     public void accept(ConsumerRecord<?, ?> consumerRecord, Exception e) {
-        System.out.println("we have reached the ConsumerRecordRecoverer for the message: " + consumerRecord.value());
+        System.out.println("we have reached the ConsumerRecordRecoverer for the message: " +
+                new String (((DeserializationException) e.getCause()).getData()));
     }
 }
